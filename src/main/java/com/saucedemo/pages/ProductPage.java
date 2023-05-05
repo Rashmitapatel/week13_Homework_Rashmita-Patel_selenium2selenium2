3609b49f00c7d8736409e81a2fcf11044fa0fd9a
Rashmitapatel <ras5mitapate1@gmail.com>
2023-05-05 19:33:40 +0100

    My Homework RashmitaPatel week13

---
+++ src/main/java/com/saucedemo/pages/ProductPage.java
@@ -0,0 +1,21 @@
+package com.saucedemo.pages;
+
+import com.saucedemo.utilities.Utility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class ProductPage extends Utility {
+    By errorProductsText = By.xpath("//div/span[text()='Products']");
+    By errorProductNumber = By.className("inventory_item");
+    public String getErrorText(){
+        return getTextFromElement(errorProductsText);
+    }
+
+    public int getProductsError(){
+        List<WebElement> list = driver.findElements(errorProductNumber);
+        int actualNumber = list.size();
+        return actualNumber;
+    }
+}
